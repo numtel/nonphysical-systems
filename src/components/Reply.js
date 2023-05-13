13d83a13584efd56a02e36ea1ea88d8ec98a5647
numtel <ben@latenightsketches.com>
2023-05-12 22:30:48 -0700

    Fix reply wonkiness
    * Also add IMessageEditable form
    * TODO add useWaitForTransaction

--- src/components/Reply.js
+++ src/components/Reply.js
@@ -1,24 +1,22 @@
 import React, { useState } from 'react';
-import { useContractWrite, usePrepareContractWrite } from 'wagmi';
+import { useContractWrite } from 'wagmi';
 
 import {contracts} from '../contracts.js';
 
 // TODO allow choosing which type of post to create
 export default function Reply({ item }) {
-  const [messageText, setMessageText] = useState('');
-  const submitReply = async (event) => {
+  const submitReply = (event) => {
     event.preventDefault();
-    // TODO fix this wonkiness
-    setMessageText(event.target.message.value);
-    await refetch();
-    write?.();
+    write({
+      recklesslySetUnpreparedArgs: [event.target.message.value, item]
+    });
   };
-  const { config, refetch, isError } = usePrepareContractWrite({
+  const { data, isLoading, isSuccess, write } = useContractWrite({
     ...contracts.PostV1Factory,
     functionName: 'createNew',
-    args: [ messageText, item ],
+    mode: 'recklesslyUnprepared',
+    args: ['', item],
   });
-  const { data, isLoading, isSuccess, write } = useContractWrite(config);
   return (
     <form onSubmit={submitReply}>
       <fieldset>
--- src/interfaces/IMessageEditable.js
+++ src/interfaces/IMessageEditable.js
@@ -1,8 +1,42 @@
+import React, { useState } from 'react';
+import { useContractWrite, useAccount } from 'wagmi';
+
 import UserBadge from '../components/UserBadge.js';
 
-export default function IMessageEditable({ owner, lastEdited }) {
+import abi from './IMessageEditable.json'
+
+export default function IMessageEditable({ owner, lastEdited, message, item }) {
+  const { address } = useAccount();
+  const [messageText, setMessageText] = useState(message);
+  const [showEdit, setShowEdit] = useState(false);
+
+  const submitEdit = async (event) => {
+    event.preventDefault();
+    write({
+      recklesslySetUnpreparedArgs: [event.target.message.value]
+    });
+  };
+
+  const { data, isLoading, isSuccess, write } = useContractWrite({
+    address: item,
+    abi,
+    functionName: 'editMessage',
+    mode: 'recklesslyUnprepared',
+    args: [ '' ],
+  });
+
   return (<>
     <UserBadge address={owner}></UserBadge>
     <p className="last-edited">Last Edited: {lastEdited.eq('0') ? "Never" : new Date(lastEdited.toString() * 1000).toLocaleString()}</p>
+    {address && address.toLowerCase() === owner.toLowerCase() && (<button onClick={() => setShowEdit(!showEdit)}>Edit</button>)}
+    {showEdit && (
+      <form onSubmit={submitEdit}>
+        <fieldset>
+          <legend>Edit Message</legend>
+          <textarea name="message" value={messageText} onChange={(e) => setMessageText(e.target.value)}></textarea>
+          <button type="submit">Submit</button>
+        </fieldset>
+      </form>
+    )}
   </>);
 }
